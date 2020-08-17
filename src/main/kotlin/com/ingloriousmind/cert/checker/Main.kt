27663edb4d2e8992a0c6f6ee04cabdcc7c7ff955
Lavong Soysavanh <lavong.soysavanh@gmail.com>
2020-08-17 20:38:26 +0200

    add basic setup, add helloworld command

---
+++ src/main/kotlin/com/ingloriousmind/cert/checker/Main.kt
@@ -0,0 +1,39 @@
+@file:JvmName("Main")
+
+package com.ingloriousmind.cert.checker
+
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.NoOpCliktCommand
+import com.github.ajalt.clikt.core.subcommands
+import com.github.ajalt.clikt.parameters.options.default
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.types.int
+
+fun main(vararg args: String) {
+	NoOpCliktCommand(name = "cert-checker")
+		.subcommands(
+			HelloCommand()
+		)
+		.main(args)
+}
+
+private class HelloCommand : CliktCommand(
+	name = "hello",
+	help = "Prints hello world"
+) {
+	val count: Int by option(
+		names = *arrayOf("--count", "-c"),
+		help = "Number of greetings",
+	).int().default(1)
+
+	val name: String by option(
+		names = *arrayOf("--name", "-n"),
+		help = "The person to greet",
+	).default("World")
+
+	override fun run() {
+		repeat(count) {
+			echo("Hello $name!")
+		}
+	}
+}
