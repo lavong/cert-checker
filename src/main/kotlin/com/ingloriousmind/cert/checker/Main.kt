440e3795c8fe060f120b7a7057e2702c6533558d
Lavong Soysavanh <lavong.soysavanh@gmail.com>
2020-12-04 19:18:09 +0100

    add certs command

--- src/main/kotlin/com/ingloriousmind/cert/checker/Main.kt
+++ src/main/kotlin/com/ingloriousmind/cert/checker/Main.kt
@@ -2,12 +2,8 @@
 
 package com.ingloriousmind.cert.checker
 
-import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.core.NoOpCliktCommand
 import com.github.ajalt.clikt.core.subcommands
-import com.github.ajalt.clikt.parameters.options.default
-import com.github.ajalt.clikt.parameters.options.option
-import com.github.ajalt.clikt.parameters.types.int
 
 fun main(vararg args: String) {
 	NoOpCliktCommand(name = "cert-checker")
@@ -16,25 +12,4 @@ fun main(vararg args: String) {
 			PublicKeyHashCommand(),
 		)
 		.main(args)
-}
-
-private class HelloCommand : CliktCommand(
-	name = "hello",
-	help = "Prints hello world"
-) {
-	val count: Int by option(
-		names = arrayOf("--count", "-c"),
-		help = "Number of greetings",
-	).int().default(1)
-
-	val name: String by option(
-		names = arrayOf("--name", "-n"),
-		help = "The person to greet",
-	).default("World")
-
-	override fun run() {
-		repeat(count) {
-			echo("Hello $name!")
-		}
-	}
 }
--- src/main/kotlin/com/ingloriousmind/cert/checker/PublicKeyHashCommand.kt
+++ src/main/kotlin/com/ingloriousmind/cert/checker/PublicKeyHashCommand.kt
@@ -1,57 +1,84 @@
 package com.ingloriousmind.cert.checker
 
 import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.arguments.convert
+import com.github.ajalt.clikt.parameters.arguments.default
+import com.github.ajalt.clikt.parameters.arguments.validate
+import com.github.ajalt.clikt.parameters.options.default
+import com.github.ajalt.clikt.parameters.options.flag
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.types.int
 import okhttp3.*
+import okhttp3.HttpUrl.Companion.toHttpUrl
+import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
 import okhttp3.logging.HttpLoggingInterceptor
 import okio.ByteString.Companion.toByteString
 import java.io.IOException
+import java.net.InetSocketAddress
+import java.net.Proxy
 import java.security.cert.X509Certificate
+import javax.net.ssl.SSLPeerUnverifiedException
+import kotlin.system.exitProcess
 
 class PublicKeyHashCommand : CliktCommand(
-	name = "hash",
+	name = "certs",
 	help = "Prints public key hash"
 ) {
+	private val url: String by argument(name = "url", help = "The url to fetch")
+		.convert { if (it.startsWith("https://")) it else "https://$it" }
+
+	private val verbose by option("-v", "--verbose", help = "Display verbose information").flag(default = false)
+
+	private val proxyHost by option("-proxy", "--proxy-host", help = "Proxy hostname to use")
+	private val proxyPort by option("-port", "--proxy-port", help = "Proxy port to use").int().default(8080)
+
 	override fun run() {
-		val request = Request.Builder()
-			.url("https://www.ingloriousmind.com/ttab/config.json")
-			.build()
-		val pinner = CertificatePinner.Builder()
-			.add("*.ingloriousmind.com", "sha256/ABC")
+		val request = Request.Builder().url(url).build()
+		val client = OkHttpClient.Builder()
+			.apply { proxyHost?.let { proxy(Proxy(Proxy.Type.HTTP, InetSocketAddress(it, proxyPort))) } }
 			.build()
 
-		val client = OkHttpClient.Builder()
-			.addInterceptor(HttpLoggingInterceptor().apply { setLevel(HttpLoggingInterceptor.Level.BODY) })
-			.certificatePinner(pinner)
-			.build()
-		
 		client.newCall(request).enqueue(object : Callback {
 			override fun onFailure(call: Call, e: IOException) {
-				echo(message = "onFailure: ${e.message}", err = true)
-				e.printStackTrace()
+
+				if (e is SSLPeerUnverifiedException) {
+					echo("peer certificate chain:")
+					e.message?.lines()
+						?.filter { it.contains("sha256/") }
+						?.onEach { echo(it) }
+						?: echo("none")
+				} else {
+					echo(message = e.message, err = true)
+				}
+
+				exitProcess(1)
 			}
 
 			override fun onResponse(call: Call, response: Response) {
-				echo("onResponse")
-				echo("handshake: ${response.handshake?.toString()}")
 
-				echo("peer certificates:")
+				echo("\npeer certificate chain:")
 				response.handshake
 					?.peerCertificates
 					?.mapNotNull { it as? X509Certificate }
-					?.forEach { x509 ->
-						echo("  ${CertificatePinner.pin(x509)} | ${x509.subjectDN}")
-						echo("  | notBefore=${x509.notBefore}")
-						echo("  | notAfter=${x509.notAfter}")
-						val validationError: String? = try {
-							x509.checkValidity()
-							null
-						} catch (t: Throwable) {
-							t.message
+					?.onEach { x509 -> echo("  ${CertificatePinner.pin(x509)} | ${x509.subjectX500Principal.name}") }
+
+				if (verbose) {
+					response.handshake?.let { echo("\nHandshake:\n$it") }
+
+					response.handshake
+						?.peerCertificates
+						?.mapNotNull { it as? X509Certificate }
+						?.forEach { x509 ->
+							echo("\n----- X509 Certificate -----\n$x509")
 						}
-						echo("  | valid=${validationError == null} ${validationError?.let { "error=$it" } ?: ""}")
-					}
+				}
+
+				exitProcess(0)
 			}
 		})
-		println("coming soon (okhttp version=${OkHttp.VERSION})")
+
+		echo("connecting to '$url'...")
+		client.proxy?.let { echo("(via proxy $it)") }
 	}
 }
