a87e895f6b0afc529fa16b5c6e579c8dcd56c70b
Lavong Soysavanh <lavong.soysavanh@gmail.com>
2022-12-11 18:05:43 +0100

    add version option

--- src/main/kotlin/com/ingloriousmind/cert/checker/Main.kt
+++ src/main/kotlin/com/ingloriousmind/cert/checker/Main.kt
@@ -4,6 +4,7 @@ package com.ingloriousmind.cert.checker
 
 import com.github.ajalt.clikt.core.NoOpCliktCommand
 import com.github.ajalt.clikt.core.subcommands
+import com.github.ajalt.clikt.parameters.options.versionOption
 
 fun main(vararg args: String) {
 	NoOpCliktCommand(name = "cert-checker")
@@ -11,5 +12,6 @@ fun main(vararg args: String) {
 			HelloCommand(),
 			FetchCertificatesCommand(),
 		)
+		.versionOption("0.2.0")
 		.main(args)
 }
